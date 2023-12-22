33d5f0d0494bd4d9470c107a93de16f8c768f998
GitHub Action <action@github.com>
2023-12-22 09:02:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/ezequielflores/personalthemelight/MyBundle.kt
+++ src/main/kotlin/com/github/ezequielflores/personalthemelight/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ezequielflores.personalthemelight
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ezequielflores/personalthemelight/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/ezequielflores/personalthemelight/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ezequielflores.personalthemelight.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
